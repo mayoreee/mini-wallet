70772c515d2b9e6c98f1c9442c0108df6aeae5b8
mayoreee <mayoreee1@gmail.com>
2022-10-09 17:27:41 +0100

    docs: send btc in `full` & `watch-only` modes

--- examples/send_btc_full.rs
+++ examples/send_btc_full.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use mini_wallet::{Network, Wallet};
+
+fn main() {
+    let descriptor = "wpkh([e9824965/84'/1'/0']tprv8fvem7qWxY3SGCQczQpRpqTKg455wf1zgixn6MZ4ze8gRfHjov5gXBQTadNfDgqs9ERbZZ3Bi1PNYrCCusFLucT39K525MWLpeURjHwUsfX/0/*)";
+    let change_descriptor = "wpkh([e9824965/84'/1'/0']tprv8fvem7qWxY3SGCQczQpRpqTKg455wf1zgixn6MZ4ze8gRfHjov5gXBQTadNfDgqs9ERbZZ3Bi1PNYrCCusFLucT39K525MWLpeURjHwUsfX/1/*)";
+
+    let recipient = "tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt"; // transaction recipient address
+    let amount = 1_000; // amount in satoshis
+
+    let wallet = Wallet {
+        descriptor: String::from(descriptor),
+        change_descriptor: String::from(change_descriptor),
+        network: Network::Testnet,
+    };
+
+    let txid = wallet.send_btc(recipient, amount).unwrap();
+
+    println!("Transaction ID: {:?}", txid);
+}
--- examples/send_btc_watch_only.rs
+++ examples/send_btc_watch_only.rs
@@ -1,1 +1,38 @@
-fn main() {}
+use mini_wallet::{Network, Wallet};
+use std::io;
+
+fn main() {
+    // Replace the descriptor with those from a wallet you control
+    let descriptor = "wpkh([bf173f25/84'/1'/0'/0/1]03ab22671edf2aa1d9d3351de5082b559c1af71c3a5227b139eb8a683782326f33)#v4eu0sl7";
+    let change_descriptor = "wpkh([bf173f25/84'/1'/0'/0/1]03ab22671edf2aa1d9d3351de5082b559c1af71c3a5227b139eb8a683782326f33)#v4eu0sl7";
+
+    let recipient = "tb1qh282uml3jkwu7c9puts00ngfzmvgrmmqaeuk3r"; // transaction recipient
+    let amount = 1_000; // amount in satoshis
+
+    let wallet = Wallet {
+        descriptor: String::from(descriptor),
+        change_descriptor: String::from(change_descriptor),
+        network: Network::Testnet,
+    };
+
+    let psbt = wallet.create_tx(recipient, amount).unwrap();
+
+    println!(
+        "The PSBT data can be found below. Copy it and sign with your external wallet \n\n{}",
+        psbt
+    );
+
+    println!("\n\nPaste the signed PSBT below\n\n");
+
+    let mut psbt_signed = String::new();
+
+    io::stdin()
+        .read_line(&mut psbt_signed)
+        .expect("Failure reading line");
+
+    let psbt_signed = psbt_signed.trim().to_string();
+
+    let txid = wallet.broadcast_tx(&psbt_signed).unwrap();
+
+    println!("Transaction ID: {:?}", txid);
+}
